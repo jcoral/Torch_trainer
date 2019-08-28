52604473dd121e7879e493b8fc6f362c58acd257
jcoral <1355539304@qq.com>
2019-08-28 20:17:26 +0800

    Add tensorboard

--- thtrainer/tensorboard.py
+++ thtrainer/tensorboard.py
@@ -4,19 +4,44 @@ from __future__ import absolute_import
 
 from thtrainer.callbacks import Callback
 from torch.utils.tensorboard import SummaryWriter
+import torch
 
 class TensorBoard(Callback):
 
-    def __init__(self, writer=None, log_dir=None, comment='', **kwargs):
-        if writer is None:
-            writer = SummaryWriter(log_dir, comment, **kwargs)
-        self.writer = writer
+    def __init__(self, log_dir=None, comment='', writer=None, input_to_model=None, **kwargs):
+        self.input_to_model = input_to_model
+        self.writer = writer or SummaryWriter(log_dir, comment, **kwargs)
 
     def on_train_begin(self, logs=None):
-        self.writer.add_graph(model=self.model.model)
+        if self.input_to_model is not None:
+            self.writer.add_graph(
+                model=self.model.model,
+                input_to_model=self.input_to_model
+            )
 
     def on_epoch_end(self, epoch, logs=None):
-        self.writer.add_scalars('Train log', logs)
+        # Split logs
+        if logs is None or len(logs) == 0:
+            return
+        metrics_logs = {metric.split(':')[-1]: {} for metric in self.params['metrics']}
+
+        for k, v in logs.items():
+            is_metric_key = False
+            seg_k = k.split(':')
+            for metric_key in metrics_logs:
+                if metric_key == seg_k[-1]:
+                    is_metric_key = True
+                    metrics_logs[metric_key][seg_k[0]] = v
+            if not is_metric_key:
+                self.writer.add_scalar(k, v, epoch)
+
+        for metric, values in metrics_logs.items():
+            self.writer.add_scalars(metric, values, epoch)
+
+        # self.writer.add_scalars('Train', logs, epoch)
+
+    def on_train_end(self, logs=None):
+        self.writer.close()
 
 
 
